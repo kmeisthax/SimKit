bc69c9296b6699d82fffaa0edc8cd64c24099f44
David Wendt (kmeisthax) <dcrkid@yahoo.com>
2013-04-02 11:43:50 -0400

    Finish SceneNode. Specifically, I:
     - Put proper namespacing on quaternion types
     - Finished get_transform
     - Added a way to manage children of SceneNodes
     - Fixed improper reference to "SceneNode" instead of "ISceneNode" in implementation file

--- include/simkit/scenenode.h
+++ include/simkit/scenenode.h
@@ -10,6 +10,7 @@ namespace SimKit {
     /* A SceneNode structures renderables within a scene.
      */
     class SIMKIT_API ISceneNode {
+        std::vector<ISceneNode*> m_children;
     protected:
         ISceneNode();
     public:
@@ -24,21 +25,26 @@ namespace SimKit {
          * A default implementation of add_transform is provided which uses get
          * and set as appropriate.
          */
-        virtual void get_transform(glm::vec4* out_pos, glm::quat* out_orientation, glm::vec3* out_scale) = 0;
-        virtual void set_transform(const glm::vec4* pos, const glm::quat* orientation, const glm::vec3* scale) = 0;
-        virtual void add_transform(const glm::vec4* pos, const glm::quat* orientation, const glm::vec3* scale);
+        virtual void get_transform(glm::vec4* out_pos, glm::glx::quaternion::quat* out_orientation, glm::vec3* out_scale) = 0;
+        virtual void set_transform(const glm::vec4* pos, const glm::glx::quaternion::quat* orientation, const glm::vec3* scale) = 0;
+        virtual void add_transform(const glm::vec4* pos, const glm::glx::quaternion::quat* orientation, const glm::vec3* scale);
         
         /* Convert the aformentioned transform into a matrix.
          */
         virtual void get_transform(glm::mat4* out_matr);
         
-        /* SceneNodes may have children SceneNodes.
+        /* SceneNodes have children.
          * 
-         * 
+         * By default, these iterators manage an internal list of child nodes.
          */
-        class iterator {
+        virtual std::vector<ISceneNode*>::iterator children_begin();
+        virtual std::vector<ISceneNode*>::iterator children_end();
+        virtual std::vector<ISceneNode*>::size_type children_size();
         
-        }
+        virtual void append_child(ISceneNode* child);
+        virtual void insert_child(ISceneNode* child, std::vector<ISceneNode*>::iterator place);
+        virtual void remove_child(ISceneNode* child);
+        virtual void remove_child(std::vector<ISceneNode*>::iterator place);
     };
 }
 
--- src/scenenode.cpp
+++ src/scenenode.cpp
@@ -1,20 +1,56 @@
 #include <simkit.h>
 
-SimKit::SceneNode::SceneNode() {};
-SimKit::SceneNode::~SceneNode() {};
+#include <glm/matrix_transform.cpp>
 
-void SimKit::SceneNode::get_transform(glm::mat4* out_matr) {
+SimKit::ISceneNode::ISceneNode() {};
+SimKit::ISceneNode::~ISceneNode() {};
+
+void SimKit::ISceneNode::get_transform(glm::mat4* out_matr) {
     if (out_matr) {
         glm::vec4 pos;
-        glm::quat orientation;
+        glm::glx::quaternion::quat orientation;
         glm::vec3 scale;
         
         this->get_transform(&pos, &orientation, &scale);
 
-        glm::mat4 pmatr = glm::translate(glm::mat4(1.0f), pos);
-        , omatr, smatr;
-        out_matr = 
+        glm::mat4 tmatr = glm::gtc::matrix_transform::translate(glm::mat4(1.0f), pos);
+        glm::mat4 omatr = glm::gtx::quaternion::toMat4(orientation);
+        glm::mat4 smatr = glm::gtc::matrix_transform::scale(glm::mat4(1.0f), scale);
+        *out_matr = tmatr * smatr * omatr;
     }
-}
+};
+
+std::vector<ISceneNode*>::iterator SimKit::ISceneNode::children_begin() {
+    return this->m_children.begin();
+};
+
+std::vector<ISceneNode*>::iterator SimKit::ISceneNode::children_end() {
+    return this->m_children.end();
+};
+
+std::vector<ISceneNode*>::size_type SimKit::ISceneNode::children_size() {
+    return this->m_children.size();
+};
+
+void SimKit::ISceneNode::append_child(SimKit::ISceneNode* child) {
+    this->m_children.push_back(child);
+};
+
+void SimKit::ISceneNode::insert_child(SimKit::ISceneNode* child, std::vector<SimKit::ISceneNode*>::iterator place) {
+    this->m_children.insert(child, place);
+};
+
+void SimKit::ISceneNode::remove_child(std::vector<SimKit::ISceneNode*>::iterator place) {
+    this->m_children.erase(place);
+};
+
+void SimKit::ISceneNode::remove_child(SimKit::ISceneNode* child) {
+    for (std::vector<SimKit::ISceneNode*>::iterator i = this->children_begin(); i != this->children_end(); i++) {
+        if (*i == child) {
+            this->remove_child(i);
+            return;
+        }
+    }
+};
 
 #endif
